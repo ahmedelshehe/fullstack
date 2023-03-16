9d3036d7a87451f1466e944c58d55ad276c3714a
Ahmed Ayman <ahmed.el-shehy@student.guc.edu.eg>
2023-03-16 02:47:55 +0200

    Finishe Part 1 Exercises

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,14 +17,14 @@ const Statistics =({good,neutral,bad,all,average,positive})=>{
     <>
       <h1>statistics</h1>
       <table>
-      <tbody>
-        <StatisticLine text="good" value={good}/>
-        <StatisticLine text="neutral" value={neutral}/>
-        <StatisticLine text="bad" value={bad}/>
-        <StatisticLine text="all" value={all}/>
-        <StatisticLine text="average" value={average}/>
-        <StatisticLine text="positive" value={positive}/>
-      </tbody>
+        <tbody>
+          <StatisticLine text="good" value={good}/>
+          <StatisticLine text="neutral" value={neutral}/>
+          <StatisticLine text="bad" value={bad}/>
+          <StatisticLine text="all" value={all}/>
+          <StatisticLine text="average" value={average}/>
+          <StatisticLine text="positive" value={positive}/>
+        </tbody>
       </table>
       
     </>
